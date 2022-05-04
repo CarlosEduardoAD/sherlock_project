62ebaa5064f51c6025218d78d813791bb7c9243a
karl <88538786+Karl-developer@users.noreply.github.com>
2022-05-04 14:29:04 -0300

    Integração CI/CD Github actions e Railway

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import pytest
 import main
+import asyncio
 
+def async_test(coro):
+    def wrapper(*args, **kwargs):
+        loop = asyncio.new_event_loop()
+        try:
+            return loop.run_until_complete(coro(*args, **kwargs))
+        finally:
+            loop.close()
+    return wrapper
+
+@async_test
 def TestConn():
     assert main.on_ready() == True
